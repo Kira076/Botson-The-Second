cfc57d67bf8a90cb25a835d67c8f08a7c45f1627
kira076 <j.morrisoncleary@gmail.com>
2020-07-17 03:15:51 +1000

    Update dice-helpers.js
    started creating the dice parser

--- utils/dice-helpers.js
+++ utils/dice-helpers.js
@@ -2,6 +2,25 @@ class DiceParser {
     constructor(inputString) {
         this.input = inputString;
     }
+    process(expression) {
+        // if brackets then call process(bracketExpression)
+        // -- while
+        // once brackets are finished, check for multiply and divide and call process(multOrDivExpression)
+        // once multiplication and division are done, check for 
+        while(expression.indexOf('(') > 0) {
+            // check for nested parens
+            const start = expression.indexOf('(');
+            const finish = expression.indexOf(')');
+            const enclosedExpression = expression.slice(start, (finish + 1));
+            // const result = process(enclosedExpression);
+        }
+    }
+}
+
+class Calculator {
+    constructor() {
+
+    }
 }
 
 module.exports = {
