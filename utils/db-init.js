3e358d196db806f1c31099acc45189b64a933293
kira076 <j.morrisoncleary@gmail.com>
2020-08-09 01:58:12 +1000

    Update db-init.js

--- utils/db-init.js
+++ utils/db-init.js
@@ -29,8 +29,9 @@ const alt_fax_funcs = {
             // db.devLogger.debug(`Res returned from alt-fax get: ${res}`);
             console.log(JSON.stringify(res.rows));
             console.log(JSON.stringify(res.rows[0]));
-            console.log(JSON.stringify(res.rows[0])['facts']);
-            facts = JSON.parse(res.rows[0])['facts'];
+            const row = JSON.parse(res.rows[0]);
+            console.log(row['facts']);
+            facts = row['facts'];
         }
         catch (err) {
             db.logger.error(`Failed to find an alt facts object for guild: ${guild} with error: ${err}`);
