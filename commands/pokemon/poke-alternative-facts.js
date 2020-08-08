9546d2d686b082476b61af612970ad26dc829487
kira076 <j.morrisoncleary@gmail.com>
2020-08-09 02:09:18 +1000

    Update poke-alternative-facts.js

--- commands/pokemon/poke-alternative-facts.js
+++ commands/pokemon/poke-alternative-facts.js
@@ -26,6 +26,7 @@ module.exports = class PokeAltFacts extends Command {
         (async () => {
             console.log('This code should execute.');
             factsObj = await this.client.dbHandler.get(guild, 'poke-alt-facts');
+            console.log(JSON.stringify(factsObj));
         })();
         console.log(factsObj);
 
