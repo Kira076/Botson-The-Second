ecc90064f09f8eba4e5e73e25e32583f23ddd596
kira076 <j.morrisoncleary@gmail.com>
2020-09-10 01:37:03 +1000

    Create way-too-specific.js

---
+++ commands/utilities/way-too-specific.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class MeowCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'way-too-specific',
+            aliases: [],
+            group: 'utilities',
+            memberName: 'way-too-specific',
+            description: 'For what purpose has god forsaken us so?',
+            args: [
+            ],
+            guildOnly: true,
+            throttling: {
+                usages: 2,
+                duration: 10,
+            },
+            // https://discord.js.org/#/docs/main/stable/class/Permissions?scrollTo=s-FLAGS
+            clientPermissions: ['ADMINISTRATOR'],
+            userPermissions: ['ADMINISTRATOR'],
+            ownerOnly: true,
+        });
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    run(message) {
+        const channel = message.channel.guild.channels.cache.get('626890547532922900');
+        const messages = channel.messages.cache;
+        console.log(messages);
+        return message.say('Face now the true meaning of insanity.');
+    }
+};
